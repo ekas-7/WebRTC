8a5170deb549e60926d185e821c2fa55c901c258
ekas-7 <ekaspreetatwal@gmail.com>
2025-02-19 18:37:01 +0530

    added frontend intial

---
+++ frontend/src/components/Reciver.tsx
@@ -0,0 +1,10 @@
+const Receiver = () => {
+    return (
+      <div className="p-4 bg-gray-100 rounded-lg shadow-md">
+        <h2 className="text-lg font-semibold">Reciver Component</h2>
+        <p className="text-gray-700">This is a simple React component.</p>
+      </div>
+    );
+  };
+  
+  export default Receiver;
